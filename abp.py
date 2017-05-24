ab50eabc32a0318b77a1928e00aa0bf8472ea5bf
Julien Dau <julien.dau@student.htw-berlin.de>
2017-05-24 23:39:48 +0200

    Trying to make use of that display finally

--- abp.py
+++ abp.py
@@ -9,25 +9,65 @@ import RPi.GPIO as gpio
 from rotaryencoder import RotaryEncoder
 from pushbutton import PushButton
 from vlcplayer import VLCPlayer
+from playerdisplay import PlayerDisplay
 
 vlc = None
 files = []
+oled = None
 running = False
 proceed = False
 
-def read_mp3s():
+#################
+###   FUNCS   ###
+#################
+
+def signal_handler(signal, frame):
+    global running
+    running = False
+
+def init_files(directory):
     global files
-    for file in os.listdir("mp3"):
-        if file.endswith(".mp3") or file.endswith(".ogg"):
-            files.append(os.path.join("mp3", file))
+    for f in os.listdir(directory):
+        if f.endswith(".mp3") or f.endswith(".ogg"):
+            files.append(os.path.join(directory, f))
     files.sort()
 
 def init_vlc():
     global vlc
-    global files
+    #global files
     vlc = VLCPlayer(100)
-    vlc.load_all(files)
+    #vlc.load_all(files)
     vlc.set_callback_track_end(on_track_end)
+
+def init_inputs():
+    global gpio
+    gpio.setmode(gpio.BCM)
+
+    btn_l_pin = 24  # left button
+    btn_l_bnc = 300
+    btn_c_pin = 23  # center button
+    btn_c_bnc = 300
+    btn_r_pin = 17  # right button
+    btn_r_bnc = 300
+    btn_p_pin = 16  # pos rotary button
+    btn_p_bnc = 300
+    btn_v_pin = 26  # vol rotary button
+    btn_v_bnc = 300 
+    rot_p_pin1 =  7 # pos rotary encoder
+    rot_p_pin2 =  1
+    rot_v_pin1 = 11 # vol rotary encoder
+    rot_v_pin2 =  0
+
+    btn_l = PushButton(gpio, btn_l_pin, btn_l_bnc, btn_l_action, name="btn_l")
+    btn_c = PushButton(gpio, btn_c_pin, btn_c_bnc, btn_c_action, name="btn_c")
+    btn_r = PushButton(gpio, btn_r_pin, btn_r_bnc, btn_r_action, name="btn_r")
+    btn_p = PushButton(gpio, btn_p_pin, btn_p_bnc, btn_p_action, name="btn_p")
+    btn_v = PushButton(gpio, btn_v_pin, btn_v_bnc, btn_v_action, name="btn_v")
+    rot_p = RotaryEncoder(gpio, rot_p_pin1, rot_p_pin2, rot_p_action, name="rot_p")
+    rot_v = RotaryEncoder(gpio, rot_v_pin1, rot_v_pin2, rot_v_action, name="rot_v")    
+
+def init_oled():
+    self.oled = PlayerDisplay("./fonts/")
 
 def print_vlc_info():
     global vlc
@@ -42,19 +82,49 @@ def print_vlc_info():
     else:
         print("Status: stopped/paused")
 
+def display_loading_screen():
+    if not self.oled:
+        return
+    w = self.oled.get_num_chars_per_line()
+    h = self.oled.get_num_lines()
+                              #####################
+    self.oled.draw_string(3, "       LOADING       ")
+    self.oled.display()
+
+def display_main_screen():
+    if not self.oled:
+        return
+    w = self.oled.get_num_chars_per_line()
+    h = self.oled.get_num_lines()
+
+    status = "[stopped]"
+    if vlc.is_playing():
+        status = "[playing]"
+
+    self.oled.draw_string(0, vlc.get_author()[:w])
+    self.oled.draw_string(1, vlc.get_title()[:w])
+    self.oled.draw_string(2, "---------------------")
+    self.oled.draw_string(2, "File: " + str(vlc.get_current()) + " / " + str(vlc.num_files()))
+    self.oled.draw_string(3, "Time: " + str(vlc.get_position()) + " / " + str(vlc.get_duration()))
+    self.oled.draw_string(4, "---------------------")
+    self.oled.draw_string(5, status + " | Vol.: " + str(vlc.get_volume()))
+    self.oled.draw_string(6, "---------------------")
+    self.oled.draw_string(7, "SEEK  <<  |>  >>  VOL")
+    self.oled.display()
+
+draw.text((2, -4), "  Audio Book Player  ", font=font, fill=255)
+draw.text((2,  4), "---------------------", font=font, fill=255)
+draw.text((2, 12), "12/114            85%", font=font, fill=255)
+draw.text((2, 20), "    8:23 / 15:67     ", font=font, fill=255)
+draw.text((2, 28), "    Stephen King     ", font=font, fill=255)
+draw.text((2, 36), "   The Dark Tower    ", font=font, fill=255)
+draw.text((2, 44), "---------------------", font=font, fill=255)
+draw.text((2, 52), "SEEK  <<  |>  >>  VOL", font=font, fill=255)
+
 def on_track_end(event):
+    # We can't call into libVLC from within the callback, so we'll take a detour
     global proceed
-    print("Track ended, scheduling next track")
     proceed = True
-
-def signal_handler(signal, frame):
-    global running
-    running = False
-
-def cleanup():
-    global vlc
-    gpio.cleanup()
-    vlc.cleanup()
 
 def btn_r_action(pin, event):
     global vlc
@@ -115,64 +185,45 @@ def rot_p_action(event):
     else:
         rot_debug(event)
 
+def cleanup():
+    global vlc
+    gpio.cleanup()
+    vlc.cleanup()
+
+
+################
+###   MAIN   ###
+################
+
+# Make sure CTRL+C will lead to a cleanup
 signal.signal(signal.SIGINT, signal_handler)
 
-read_mp3s()
+print("Initializing GPIO...")
+init_inputs()
+print("Initializing OLED display...")
+init_oled()
+print("Displaying loading screen...")
+display_loading_screen()
+print("Scanning for audio files...")
+init_files()
 
-sys.stdout.write("Initializing audio player... ")
-init_vlc()
-sys.stdout.write("done\n")
-sys.stdout.flush()
-
-running = True
-
-sys.stdout.write("Initializing GPIO... ")
-
-btn_l_pin = 24  # left button
-btn_l_bnc = 300
-
-btn_c_pin = 23  # center button
-btn_c_bnc = 300
-
-btn_r_pin = 17  # right button
-btn_r_bnc = 300
-
-btn_p_pin = 16  # pos rotary button
-btn_p_bnc = 300
-
-btn_v_pin = 26  # vol rotary button
-btn_v_bnc = 300 
-
-rot_p_pin1 =  7 # pos rotary encoder
-rot_p_pin2 =  1
-
-rot_v_pin1 = 11 # vol rotary encoder
-rot_v_pin2 =  0
-
-gpio.setmode(gpio.BCM)
-
-btn_l = PushButton(gpio, btn_l_pin, btn_l_bnc, btn_l_action, name="btn_l")
-btn_c = PushButton(gpio, btn_c_pin, btn_c_bnc, btn_c_action, name="btn_c")
-btn_r = PushButton(gpio, btn_r_pin, btn_r_bnc, btn_r_action, name="btn_r")
-btn_p = PushButton(gpio, btn_p_pin, btn_p_bnc, btn_p_action, name="btn_p")
-btn_v = PushButton(gpio, btn_v_pin, btn_v_bnc, btn_v_action, name="btn_v")
-rot_p = RotaryEncoder(gpio, rot_p_pin1, rot_p_pin2, rot_p_action, name="rot_p")
-rot_v = RotaryEncoder(gpio, rot_v_pin1, rot_v_pin2, rot_v_action, name="rot_v")
-
-sys.stdout.write("done\n")
-sys.stdout.flush()
-
-print_vlc_info()
+if len(files) > 0:
+    print("Found " + str(len(files)) + " files.")
+    print("Initializing VLC...")
+    init_vlc()
+    print("Loading audio files...")
+    vlc.load_all(files)
+    running = True
 
 while running:
+    display_main_screen()
     if proceed:
+        print("Track ended, scheduling next track")
         proceed = False
         vlc.next()
         vlc.play()
     time.sleep(1)
 
-sys.stdout.write("Shutdown initialized... ")
+print("Shutting down, bye bye!")
 cleanup()
-sys.stdout.write("done. Bye!\n")
-sys.stdout.flush()
 sys.exit(0)
---
+++ playerdisplay.py
@@ -0,0 +1,76 @@
+from display import Display
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+class PlayerDisplay(Display):
+
+    ADDRESS = 0x3C
+    RST_PIN = 4
+
+    FONT_FILE = "cellphone_6px.ttf"
+    FONT_SIZE = 16
+
+    LINE_HEIGHT = 8
+    CHAR_WIDTH  = 6
+
+    START_LEFT =  1
+    START_TOP  = -4
+
+    def __init__(self, font_dir="./"):
+        self.oled = Adafruit_SSD1306.SSD1306_128_64(rst=PlayerDisplay.RST_PIN, i2c_address=PlayerDisplay.ADDRESS)
+        self.oled.begin()
+        self.oled.clear()
+        self.oled.display()
+        self.width = self.oled.width
+        self.height = self.oled.height
+        self.image = Image.new('1', (self.width, self.height)) # 1 = 1-bit color
+        self.draw = ImageDraw.Draw(self.image)
+        self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0) # TODO is this needed?
+        self.font = ImageFont.truetype(font_dir + PlayerDisplay.FONT_FILE, PlayerDisplay.FONT_SIZE)
+
+    # Return display width in pixels
+    def get_width(self):
+        return self.width
+
+    # Return display height in pixels
+    def get_height(self):
+        return self.height
+
+    # Return the number of characters that
+    # can be displayed in one line
+    def get_num_chars_per_line(self):
+        return 21 # TODO calc this instead
+
+    # Return the number of lines
+    def get_num_lines(self):
+        return 8 # TODO calc this instead
+
+    # Render the given string in the given line
+    # String should only contain ASCII and should
+    # not exceed get_num_chars_per_line() length
+    def draw_string(self, line, text, align=None):
+        x = PlayerDisplay.START_LEFT
+        y = PlayerDisplay.START_TOP + (line * PlayerDisplay.LINE_HEIGHT)
+        self.draw.text((x, y), text[:self.get_num_chars_per_line()], font=self.font, fill=255)
+
+    # Render the given char in the given line/pos
+    def draw_char(self, line, pos, char):
+        x = PlayerDisplay.START_LEFT + (pos * PlayerDisplay.CHAR_WIDTH)
+        y = PlayerDisplay.START_TOP + (line * PlayerDisplay.LINE_HEIGHT)
+        self.draw.text((x, y), char[:1], font=self.font, fill=255)
+
+    def clear(self):
+        self.oled.clear()
+        self.oled.display()
+
+    def display(self):
+        self.oled.image(self.image)
+        self.oled.display()
+
+    def cleanup(self):
+        self.clear()
